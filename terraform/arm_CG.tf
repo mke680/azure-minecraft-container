6952ac162dc1655ed230d37b16b46ac15ed12332
Michael Cho <55390802+mke680@users.noreply.github.com>
2020-10-25 21:53:30 +1100

    Update arm_CG.tf

--- terraform/arm_CG.tf
+++ terraform/arm_CG.tf
@@ -1,15 +1,20 @@
+# Configure the Docker provider
+provider "docker" {
+  host = "ssh://user@remote-host:22"
+}
+
 # Azure Container Group
 resource "azurerm_container_group" "minecraft_container" {
   name                = "craftlabs_papermc"
   location            = azurerm_resource_group.minecraft_container.location
   resource_group_name = azurerm_resource_group.minecraft_container.name
   ip_address_type     = "public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "craftlabs"
   os_type             = "Linux"
 
   # Container One
   container {
-    name   = "hello-world"
+    name   = "craftlabs01"
     image  = var.docker_image
     cpu    = "1"
     memory = "2"
